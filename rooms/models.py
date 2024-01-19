56329e80f2959c7673af2fb932ce62bd06712135
SangHyunJeong <jsh900118@gmail.com>
2024-01-20 00:06:31 +0900

    6-4

--- rooms/models.py
+++ rooms/models.py
@@ -10,6 +10,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveBigIntegerField()
@@ -21,6 +22,9 @@ class Room(CommonModel):
     kind = models.CharField(max_length=20, choices = RoomKindChoices.choices)
     owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
     amenities = models.ManyToManyField("rooms.Amenity")
+
+    def __str__(self) -> str:
+        return self.name
     
 
 class Amenity(CommonModel):
@@ -29,3 +33,9 @@ class Amenity(CommonModel):
 
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=150, null=True, blank=True,)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Amenities"
